a70d913c95a915b6e254b12288ed34c7c5a94775
jesusbeb <jbeltranb@outlook.com>
2022-05-30 18:02:30 -0500

    Tercera vez agregando Actions

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -11,7 +11,7 @@ describe("Tests para ExplorerService", () => {
     test("2. Requerimiento: Calcular todos los explorers en la mision 'node' del archivo 'explorers.json', probando getAmountOfExplorersByMission", () => {
         const explorers= Reader.readJsonFile("explorers.json") // se declara una constante que llama a la funcion static "readJsonFile" de la clase "Reader" y le manda el path de explorers.json para que lo lea
         const explorersInNode= ExplorerService.getAmountOfExplorersByMission(explorers,"node"); // constante que llama a la funcion "filterByMission" de la clase "ExplorerService" y le envia los parametros "explorers" y "node"
-        expect(explorersInNode).toBe(9);
+        expect(explorersInNode).toBe(10);
     })
 
     test("3. Requerimiento: Probar la funcion 'getExplorersUsernamesByMission'", () => {
