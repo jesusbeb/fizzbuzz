b6ef9b5c1606e35a71b51ae67c60f5bef7c4d794
jesusbeb <jbeltranb@outlook.com>
2022-06-07 10:47:47 -0500

    Creando un server básico

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -2,7 +2,7 @@ const ExplorerController= require('./../../lib/controllers/ExplorerController');
 
 describe("Test para ExplorerController", () => {
     test("1. Test para getExplorerByMission", () => {
-        const explorersInNode= ExplorerController.getExplorersByMission("node");
-        expect(explorersInNode.length).toBe(10);
+        const explorersInNode= ExplorerController.getExplorersByMission("java");
+        expect(explorersInNode.length).toBe(5);
     })
 })
